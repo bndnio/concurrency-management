abdb84949338c55d57755a32909ff625bad1b446
Brendon Earl <bearl@uvic.ca>
2018-11-22 23:28:26 -0800

    Remove option from records array

--- ByzantineGenerals/ByzantineGenerals/General.swift
+++ ByzantineGenerals/ByzantineGenerals/General.swift
@@ -14,17 +14,16 @@ enum Order {
 class General {
     let id: Int
     let traitor: Bool
-    var records: [Order]?
-    
+    var records: [Order]
+
     init(_ id: Int, traitor: Bool) {
         self.id = id
         self.traitor = traitor
+        self.records = []
     }
 
     func hearOrder(_ order: Order, _ generals: Array<General>, m: Int, from: Int?) {
-        print("General\(self.id) :: hearing \(order) order from \(from ?? -1)")
-        if records != nil { records!.append(order) }
-        else { records = [order] }
+        records.append(order)
         
         if m >= 0 {
             self.distributeOrder(generals, m: m, from: from)
@@ -40,29 +39,28 @@ class General {
     }
     
     func shareOrder(_ general: General, _ generals: Array<General>, m: Int) {
-        if self.traitor == true && (general.id+1) % 2 == 0 {
+        if self.traitor == true && (general.id) % 2 == 0 {
             switch self.majority() {
             case Order.ATTACK: general.hearOrder(Order.RETREAT, generals, m: m, from: self.id)
             case Order.RETREAT: general.hearOrder(Order.ATTACK, generals, m: m, from: self.id)
             }
         }
         else {
-            general.hearOrder(self.majority(), generals, m: m, from: self.id)
+           general.hearOrder(self.majority(), generals, m: m, from: self.id)
         }
     }
     
     func majority() -> Order {
         var attack_count = 0
         var retreat_count = 0
-        guard let records = self.records else {
-            return Order.RETREAT
-        }
+        
         for (order) in records {
             switch order {
             case Order.ATTACK: attack_count += 1
             case Order.RETREAT: retreat_count += 1
             }
         }
+        print("General\(self.id) :: [Attack: \(attack_count), Retreat: \(retreat_count)]")
         if attack_count > retreat_count { return Order.ATTACK }
         else if attack_count < retreat_count { return Order.RETREAT }
         else { return Order.RETREAT }
